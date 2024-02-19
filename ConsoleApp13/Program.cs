633420e081ca492ea5cb595e082f906ba0d299db
cerf01 <crosscrisps21@gmail.com>
2024-02-19 12:24:00 +0100

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        public static string connectionStr => "Data Source=DESKTOP-BRQ9LQE\\SQLEXPRESS;Initial Catalog=FruitAndVeg;Integrated Security=True;Connect Timeout=30;";
+        static void Main(string[] args)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionStr))
+            {
+                try
+                {
+                    connection.Open();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.WriteLine("Connected!");
+                string q = "";
+                while (q != "e")
+                {
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    Console.WriteLine("Navigation:exit, ex1,  ex2, ex3,  ex4, ex5, ex6, ex7, ex8, ex9, ex10, ex11, ex12, ex13, ex14");
+                    q = Console.ReadLine();
+                    Console.WriteLine();
+                    switch (q.ToLower())
+                    {
+                        default:
+                            Console.WriteLine("wrong input!");
+                            break;
+                        case "exit":
+                            {
+
+                                q = "e";
+                                connection.Close();
+                                Console.WriteLine("goodbye!");
+                                break;
+                            }
+                            break;
+                        case "1":
+                        case "ex1":
+                            {
+                                string query = "SELECT * FROM Info";
+                                showQuerySelect(query, connection);                                
+                                Console.ReadKey();  
+                            }
+                            break;
+                        case "2":
+                        case "ex2":
+                            {
+                                string query = "SELECT [Name] FROM Info";
+                                showQuerySelect(query, connection);                                
+                                Console.ReadKey();  
+                            }
+                            break;
+                        case "3":
+                        case "ex3":
+                            {
+                                string query = "SELECT Color FROM Info";
+                                showQuerySelect(query, connection);                                
+                                Console.ReadKey();  
+                            }
+                            break;
+                        case "4":
+                        case "ex4":
+                            {
+                                string query = "SELECT MAX(Calories) FROM Info";
+                                showQuerySelect(query, connection);                                
+                                Console.ReadKey();  
+                            }
+                            break;
+                        case "5":
+                        case "ex5":
+                            {
+                                string query = "SELECT MIN(Calories) FROM Info";
+                                showQuerySelect(query, connection);                                
+                                Console.ReadKey();  
+                            }
+                            break;
+                        case "6":
+                        case "ex6":
+                            {
+                                string query = "SELECT AVG(Calories) FROM Info";
+                                showQuerySelect(query, connection);                                
+                                Console.ReadKey();  
+                            }
+                            break;
+                        case "7":
+                        case "ex7":
+                            {
+                                string query = "SELECT COUNT(*) FROM Info WHERE [Type] = 'Veg'";
+                                showQuerySelect(query, connection);                                
+                                Console.ReadKey();  
+                            }
+                            break;
+                        case "8":
+                        case "ex8":
+                            {
+                                string query = "SELECT COUNT(*) FROM Info WHERE [Type] = 'Fruit'";
+                                showQuerySelect(query, connection);                                
+                                Console.ReadKey();  
+                            }
+                            break;
+                        case "9":
+                        case "ex9":
+                            {
+                                Console.WriteLine("Enter color: ");
+                                string inputColor = Console.ReadLine();
+                                string query = $"SELECT COUNT(*) FROM Info WHERE Color= '{inputColor.ToLower()}'";
+                                showQuerySelect(query, connection);                                
+                                Console.ReadKey();  
+                            }
+                            break;
+                        case "10":
+                        case "ex10":
+                            {
+                                string query = "SELECT Color, COUNT(*) FROM Info GROUP BY Color";
+                                showQuerySelect(query, connection);                                
+                                Console.ReadKey();  
+                            }
+                            break;
+                        case "11":
+                        case "ex11":
+                            {
+                                Console.WriteLine("Enter min calorie");
+                                int minClaories = getLineGigit(Console.ReadLine());
+
+                                string query = $"SELECT [Name],Calories FROM Info WHERE Calories < '{minClaories}' ";
+                                showQuerySelect(query, connection);                                
+                                Console.ReadKey();  
+                            }
+                            break;
+                        case "12":
+                        case "ex12":
+                            {
+                                Console.WriteLine("Enter max calorie");
+                                int maxClaories = getLineGigit(Console.ReadLine());
+
+                                string query = $"SELECT [Name],Calories FROM Info WHERE Calories > '{maxClaories}' ";
+                                showQuerySelect(query, connection);                                
+                                Console.ReadKey(); 
+                            }
+                            break;
+                        case "13":
+                        case "ex13":
+                            {
+                                Console.WriteLine("Enter min calorie");
+                                int minClaories = getLineGigit(Console.ReadLine());
+
+                                Console.WriteLine("Enter max calorie");
+                                int maxClaories = getLineGigit(Console.ReadLine());
+
+                                string query = $"SELECT [Name],Calories FROM Info WHERE Calories < '{maxClaories}' AND Calories > '{minClaories}'";
+                                showQuerySelect(query, connection);                               
+                                Console.ReadKey(); 
+    
+                            }
+                            break;
+                        case "14":
+                        case "ex14":
+                            {
+                                string query = $"SELECT [Name] FROM Info WHERE Color ='Red' OR Color = 'Yallow'";
+                                showQuerySelect(query, connection);                               
+                                Console.ReadKey(); 
+
+                            }
+                            break;
+                    }
+                }
+
+                Console.ReadKey();
+            }
+        }
+        public static void showQuerySelect(string query, SqlConnection connctinon) 
+        {
+            using (var cmd = new SqlCommand(query, connctinon))
+            {
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                        Console.Write(reader[i] + "    ");
+                    Console.WriteLine();
+                }
+                reader.Close();
+            }
+        }
+
+        public static int getLineGigit(string str)
+        {
+            string numLine = str;
+            if (numLine.All(c => char.IsDigit(c)))
+                return Convert.ToInt32(numLine);
+          
+            Console.WriteLine("is not a digit! try again");
+
+            getLineGigit(Console.ReadLine());
+            return 0;
+
+        }
+    }
+}
---
+++ ConsoleApp13/SQLQuery1.sql
@@ -0,0 +1,38 @@
+﻿CREATE DATABASE FruitAndVeg
+GO
+
+USE FruitAndVeg
+GO
+
+CREATE TABLE Info
+(
+	Id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[Name] NVARCHAR(MAX) NOT NULL,
+	[Type] NVARCHAR(MAX) NOT NULL,
+	Color NVARCHAR(MAX) NOT NULL,
+	Calories FLOAT NOT NULL
+);
+
+INSERT INTO Info VALUES
+('Apple', 'Fruit','Red',51.2)
+
+INSERT INTO Info VALUES
+('Carrot', 'Veg','Orange',41.3 )
+
+INSERT INTO Info VALUES
+('Tomato', 'Veg','Red',18.0 )
+
+INSERT INTO Info VALUES
+('Potato', 'Veg','Brown', 164.0 )
+
+INSERT INTO Info VALUES
+('Pipeapple', 'Fruit','Yallow', 50.0 )
+
+INSERT INTO Info VALUES
+('Banana', 'Fruit','Yallow', 88.7 )
+
+INSERT INTO Info VALUES
+('Cucumber', 'Veg','Green', 16.0)
+  
+ INSERT INTO Info VALUES
+('Purple Plum', 'Fruit','Purple', 35.0 )
